3a904650a41215c876fea175be8597d0156800fe
Coiseco <stepanovik12@gmail.com>
2022-05-02 17:43:49 +0300

    faq

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -7,6 +7,19 @@ $(document).ready(function () {
 
 
 
+  let prevBtn;
+
+  $('.j-accordeon-btn').on('click' , function () {
+    if(prevBtn === this){
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $('.j-accordeon-btn').next().slideUp();
+    $(this).next().slideDown();
+    prevBtn = this;
+
+  })
 
 
 
